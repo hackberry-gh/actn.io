455e1087af30b0193e6c17081424b90c0826fced
Onur Uyar <me@onuruyar.com>
2014-11-06 02:31:05 +0000

    new page, horay

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
   
-  window.setTimeout(function(){
-  actn.get({
-  path: "/signatures",
-  success: function(data){
-    if(data[0]){
-      $("#script3").val($("#script3").text().replace("...loading...",data[0]['uuid']))
+  window.onActn = function(){
+    actn.get({
+    path: "/signatures",
+    success: function(data){
+      if(data[0]){
+        $("#script3").val($("#script3").text().replace("...loading...",data[0]['uuid']))
+      }
     }
+    })
   }
-  })
-  },2000);
   
   $("button.jsRun").click(function(e){
     e.preventDefault();
@@ -17,7 +17,7 @@ $(document).ready(function(){
     eval(code);
   })
   
-  $(".tabs a").click(function(e){
+  $(".tabs a.handle").click(function(e){
     e.preventDefault();
     var $a = $(e.currentTarget);
     $(".tabs a.is-active").removeClass("is-active");
